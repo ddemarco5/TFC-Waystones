f4c20a29f376e8e1a602ada17c8fee8cbe3671e4
Dominic DeMarco <awesome3165@gmail.com>
2020-07-16 02:22:08 -0700

    add warpstone charges however, the mod doesn't behave well if we remove the warpstone after the last charge is used. Need some way to do it post-teleportation

--- src/main/java/com/ddemarco5/TFCWaystones/item/ItemWarpStone.java
+++ src/main/java/com/ddemarco5/TFCWaystones/item/ItemWarpStone.java
@@ -5,20 +5,35 @@ import net.blay09.mods.waystones.Waystones;
 import net.dries007.tfc.api.capability.size.IItemSize;
 import net.dries007.tfc.api.capability.size.Size;
 import net.dries007.tfc.api.capability.size.Weight;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.lang.reflect.Field;
+import java.util.List;
 
 public class ItemWarpStone extends net.blay09.mods.waystones.item.ItemWarpStone implements IItemSize {
 
     private static final ResourceLocation resourceOverride = new ResourceLocation(Waystones.MOD_ID, "warp_stone");
+    private static final int numCharges = 4;
 
+    public ItemWarpStone() {
+        super();
+    }
 
     public void patchResource() {
 
@@ -28,7 +43,7 @@ public class ItemWarpStone extends net.blay09.mods.waystones.item.ItemWarpStone 
             Field resourcelocationField = IForgeRegistryEntry.Impl.class.getDeclaredField("registryName");
             resourcelocationField.setAccessible(true);
             resourcelocationField.set(this, resourceOverride);
-            TFCWaystones.logger.info("Reflection patched resource {} to {}", TFCWaystones.WARP_STONE.toString(), resourceOverride.toString());
+            TFCWaystones.logger.info("Reflection patched resource {} to {}", this.toString(), resourceOverride.toString());
 
         } catch (Exception e) {
             TFCWaystones.logger.error("FOOK: {}", e);
@@ -58,5 +73,58 @@ public class ItemWarpStone extends net.blay09.mods.waystones.item.ItemWarpStone 
         OreDictionary.registerOre("warpStone", this);
         OreDictionary.registerOre("warpStoneCharged", this);
     }
+
+    // Set NBT tag when the item is crafted
+    @Override
+    public void onCreated(ItemStack stack, World worldIn, EntityPlayer playerIn) {
+        super.onCreated(stack, worldIn, playerIn);
+        makeNBT(stack);
+    }
+
+
+    @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving) {
+        ItemStack item = super.onItemUseFinish(stack, worldIn, entityLiving);
+        int chargesLeft = 0;
+        if (stack.hasTagCompound() && stack.getTagCompound().hasKey("Charges")) {
+            chargesLeft = stack.getTagCompound().getInteger("Charges");
+            stack.getTagCompound().setInteger("Charges", chargesLeft - 1);
+        }
+        else {
+            TFCWaystones.logger.error("BAD! We don't have an NBT tag when we should!!!!");
+        }
+
+        //if ((chargesLeft - 1) <= 0) {
+        //    return ItemStack.EMPTY;
+        //}
+        return item;
+
+    }
+
+    @Override
+    public boolean onDroppedByPlayer(ItemStack item, EntityPlayer player) {
+        if (player.capabilities.isCreativeMode) {
+            TFCWaystones.logger.info("In creative mode, adding charges this way");
+            makeNBT(item);
+        }
+        return super.onDroppedByPlayer(item, player);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        // Because addinformation is always run when hovering an item, we can add our nbt tag here if we don't already have it
+        if ((!stack.hasTagCompound()) || (!stack.getTagCompound().hasKey("Charges"))) {
+            makeNBT(stack);
+        }
+        tooltip.add("Charges: " + stack.getTagCompound().getInteger("Charges"));
+    }
+
+    private void makeNBT(ItemStack stack) {
+        NBTTagCompound nbt = new NBTTagCompound();
+        nbt.setInteger("Charges", numCharges);
+        stack.setTagCompound(nbt);
+    }
+
 
 }
